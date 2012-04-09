721db020c5f0ec2eaaeb0127d3dfc6ec0926ba86
Steven Lu <tacticalazn@gmail.com>
2012-04-09 10:17:33 -0400

    making differentiating model

--- application/controllers/sendit.php
+++ application/controllers/sendit.php
@@ -11,6 +11,8 @@ class Sendit extends CI_Controller {
          return;
       }
 
+      $this->load->model('sendit_model');
+
       // If any of these are empty, its bad.
       if (empty($_POST['mydoc']) || empty($_FILES['myresume']))
       {
@@ -18,92 +20,38 @@ class Sendit extends CI_Controller {
          return;
       }
 
-      // decoding data
-      $resume = $_FILES['myresume']['tmp_name'];
-      $doc = json_decode($_POST['mydoc'], TRUE);
-
-      $temp_path = "/tmp/resume-".mt_rand().".pdf";
-      
       // renaming the file
-      if (move_uploaded_file($resume, $temp_path) === FALSE)
+      if (!$resume = $this->sendit_model->move_file($_FILES['myresume']['tmp_name']))
       {
-         echo "Whoops, try again!";
+         echo "Whoops, filesystem error. Please try again!";
          return;
       }
-      
-      $resume = $temp_path;
 
       // checking the doc as JSON
-      if (empty($doc))
+      if (!$doc = $this->sendit_model->decode_doc($_POST['mydoc']))
       {
          echo "Not a proper JSON document.";
          return;
       }
 
       // checking for fields
-      if (empty($doc['full_name'])
-         || empty($doc['email_address'])
-         || empty($doc['phone_number'])
-         || empty($doc['q1'])
-         || empty($doc['q2']))
+      if (!$this->sendif_model->validate_doc($doc))
       {
-         echo "Missing fields in JSON document. Exepecting full_name, email_address, phone_number, q1, q2.";
+         echo "Missing fields in JSON document. Exepected fields: full_name email_address phone_number q1 q2";
          return;
       }
 
       // validating email
-      $this->load->helper('email');
-      if (!valid_email($doc['email_address']))
+      if (!$this->sendit_model->validate_email($doc['email_address']))
       {
          echo "Email does not seem to be valid.";
          return;
       }
 
-      //making email doc
-      $this->load->library('email');
-
-      $this->email->from($doc['email_address'], $doc['full_name']);
-      //$this->email->to('steve@noisenewyork.com');
-      $this->email->to('jobs@noisenewyork.com');
-      $this->email->subject('Software Engineer - Priority');
-
-      $message = "The applicant completed the application process.\n\n";
-      $message .= "Q. Write a snippet of code which shows a function ana variable within the same object using Javascript.\nA. ";
-      $message .= $doc['q1'];
-      $message .= "\n\nQ. Briefly explain what a MVC (Model-View-Controller) is and why it's not smart to run a a function call in a view.\nA. ";
-      $message .= $doc['q2'];
-      $message .= "\n\n";
-      $message .= "Full Name:";
-      $message .= $doc['full_name'];
-      $message .= "\nEmail Address:";
-      $message .= $doc['email_address'];
-      $message .= "\nPhone Number:";
-      $message .= $doc['phone_number'];
-
-      $this->email->message($message);
-      $this->email->attach($resume);
-
-      if (!$this->email->send())
+      if (!$this->sendit_model->send_noise_email($doc, $resume)
+         || !$this->sendit_model->send_confirmation_email($doc['email_address']))
       {
-         echo "Oops! Please try again!";
-         return;
-      }
-
-      // Sending confirmation email.
-      $this->email->clear();
-
-      $this->email->from('jobs@noisenewyork.com', 'The Noise Team');
-      $this->email->to($doc['email_address']);
-      $this->email->subject('Application Received');
-
-      $message = "We're just letting you know that we've received your application and because you've got to the end, your application has been given priority. If our team deems you as a good match, we will get in contact with you.\n\n -- The Noise Team @ New York";
-
-      $this->email->message($message);
-
-
-      if (!$this->email->send())
-      {
-         echo "Oops! Please try again!";
+         echo "Oops! Couldn't send mail. Please try again!";
          return;
       }
 
