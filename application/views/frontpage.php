98c895c3af5de0d743df999b179a82f551f9140a
Steven Lu <tacticalazn@gmail.com>
2012-10-04 12:49:17 -0400

    Fixing stuff.

--- application/views/frontpage.php
+++ application/views/frontpage.php
@@ -7,10 +7,10 @@
          <a href="https://jobs.github.com/positions/583a5b28-7a19-11e1-9520-6e45a5eca057">Web Developer</a><br />
          <a href="https://jobs.github.com/positions/4bcdabdc-4f36-11e1-9a8a-b885c8275c0f">Senior Back-End Developer</a>
       </p>
-      <p>Here, we will test your knowledge in parsing APIs and language aspects. If you find this intriguing and fun, Noise is definitely for you.</p>
+      <p>Here, we will test your knowledge in parsing APIs and language aspects. If you find this intriguing and fun, noise is definitely for you.</p>
       <h2>Getting Started</h2> 
       <p>We are giving you a key and a URL, it is up to you how to use it. It will contain the next step of this test.</p>
-      <pre>git@github.com:sjlu/Noise.git</pre>
+      <pre>git@github.com:sjlu/noise.git</pre>
       <pre>-----BEGIN DSA PRIVATE KEY-----
 MIIBugIBAAKBgQCR/55IV3MkRjeKQhG1Myib6KELH+Sgycbw3MZRJHvpM89Qk1df
 o8QBjISeYih/GVTH/9MexW5LJI1yQTSXUwAPoFS1SiOGhSwjdjVjyMLs/IFzDItO
@@ -24,6 +24,6 @@ FyUUX/fSf6ZKn9sm3qYiopyELhK7hVizCcm7hHQuvPJRjsWbMods2wIURzdro9if
 Bm8VhAgjWoeNbpwJIHM=
 -----END DSA PRIVATE KEY-----</pre>
       <h2>Stuck?</h2>
-      <p>If a position you're applying to has nothing to do with development or if you're simply stuck, you can always send us your resume at <a href="mailto:jobs@noisenewyork.com">jobs@noisenewyork.com</a> with the subject line as "Software Engineer - github" or with the appropriate position title you are applying to.</p>
+      <p>If a position you're applying for has nothing to do with development or if you're simply stuck, you can always send us your resume at <a href="mailto:jobs@noisenewyork.com">jobs@noisenewyork.com</a> with the subject line as "Software Engineer - github" or with the appropriate position title you are applying to.</p>
    </div>
 </div>
