4884fbe729799f7d71b84331f9cbc4a8c944e402
Steven Lu <tacticalazn@gmail.com>
2012-10-03 21:58:07 -0400

    Modifying.

--- application/views/frontpage.php
+++ application/views/frontpage.php
@@ -12,17 +12,17 @@
       <p>We are giving you a key and a URL, it is up to you how to use it. It will contain the next step of this test.</p>
       <pre>git@github.com:sjlu/Noise.git</pre>
       <pre>-----BEGIN DSA PRIVATE KEY-----
-      MIIBugIBAAKBgQCR/55IV3MkRjeKQhG1Myib6KELH+Sgycbw3MZRJHvpM89Qk1df
-      o8QBjISeYih/GVTH/9MexW5LJI1yQTSXUwAPoFS1SiOGhSwjdjVjyMLs/IFzDItO
-      ke33C+th823CvgDC/ZqWy2uNBe0GsRACuqXQGO3P15PcNIlDQcf7IS9ypQIVAJLC
-      kka/tq6pBtgtxdmq3XrYr4sRAoGAcZoBMoNEpJqJ8KuOdxe/5aEDVHcVDoiQnvbG
-      wrKxgSzBox4oITu5QPKrTmFdnel5rtpYiKbRLxI35UHvFmTPIObD84er/wjPxhNx
-      N1T8ekt2jFonq+gLcIo6bv03qsYXpfBY/+MRQT7WqMa0LzSMyPmQwqWjbnrdZ97Q
-      v2OZSTcCgYBDMfRrmpNjCfeNI84cTp5RUdgC+nSVWVRQ62yV5IRnry/tszDAiYXM
-      cykhTFIB8BSm1Exlj4057kc69I92NC+AiqCeSR+zbragW4Xfl7N6rxzeopuwJgqu
-      FyUUX/fSf6ZKn9sm3qYiopyELhK7hVizCcm7hHQuvPJRjsWbMods2wIURzdro9if
-      Bm8VhAgjWoeNbpwJIHM=
-      -----END DSA PRIVATE KEY-----</pre>
+MIIBugIBAAKBgQCR/55IV3MkRjeKQhG1Myib6KELH+Sgycbw3MZRJHvpM89Qk1df
+o8QBjISeYih/GVTH/9MexW5LJI1yQTSXUwAPoFS1SiOGhSwjdjVjyMLs/IFzDItO
+ke33C+th823CvgDC/ZqWy2uNBe0GsRACuqXQGO3P15PcNIlDQcf7IS9ypQIVAJLC
+kka/tq6pBtgtxdmq3XrYr4sRAoGAcZoBMoNEpJqJ8KuOdxe/5aEDVHcVDoiQnvbG
+wrKxgSzBox4oITu5QPKrTmFdnel5rtpYiKbRLxI35UHvFmTPIObD84er/wjPxhNx
+N1T8ekt2jFonq+gLcIo6bv03qsYXpfBY/+MRQT7WqMa0LzSMyPmQwqWjbnrdZ97Q
+v2OZSTcCgYBDMfRrmpNjCfeNI84cTp5RUdgC+nSVWVRQ62yV5IRnry/tszDAiYXM
+cykhTFIB8BSm1Exlj4057kc69I92NC+AiqCeSR+zbragW4Xfl7N6rxzeopuwJgqu
+FyUUX/fSf6ZKn9sm3qYiopyELhK7hVizCcm7hHQuvPJRjsWbMods2wIURzdro9if
+Bm8VhAgjWoeNbpwJIHM=
+-----END DSA PRIVATE KEY-----</pre>
       <h2>Stuck?</h2>
       <p>If a position you're applying to has nothing to do with development or if you're simply stuck, you can always send us your resume at <a href="mailto:jobs@noisenewyork.com">jobs@noisenewyork.com</a> with the subject line as "Software Engineer - github" or with the appropriate position title you are applying to.</p>
    </div>
